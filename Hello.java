7781ff28ce7234db20a727b933eb29d92062991b
Scott Cederberg <cederber@gmail.com>
2012-04-04 22:30:46 -0700

    Trying out another greeting variation.

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hi there, git users!");
+    System.out.println("Hi there, git users--welcome to awesomeness!");
   }
 }
